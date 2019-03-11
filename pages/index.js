e8647015b7947af272b4d377604b4314f4be0913
Oscar <newman.oscar@gmail.com>
2019-03-11 01:22:24 -0400

    fonts added

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,7 @@ const Index = () => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <link rel="stylesheet" href="https://use.typekit.net/xzl5vod.css" />
       </Head>
       <section className="flex-shrink flex-no-grow">
         <img
